f1f34e85ca46b840a1eba6ca5880604d559a730a
Daniel Celestino <danielcelestino@gmail.com>
2022-10-15 18:42:57 -0300

    Product and colections part 1

--- src/main/java/com/aulajpamaven/course/config/TestConfig.java
+++ src/main/java/com/aulajpamaven/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.aulajpamaven.course.entities.Category;
 import com.aulajpamaven.course.entities.Order;
+import com.aulajpamaven.course.entities.Product;
 import com.aulajpamaven.course.entities.User;
 import com.aulajpamaven.course.entities.enums.OrderStatus;
 import com.aulajpamaven.course.repositories.CategoryRepository;
 import com.aulajpamaven.course.repositories.OrderRepository;
+import com.aulajpamaven.course.repositories.ProductRepository;
 import com.aulajpamaven.course.repositories.UserRepository;
 
 @Configuration
@@ -26,6 +28,8 @@ public class TestConfig implements CommandLineRunner{ //executa automaticamente 
 	private OrderRepository orderRepository; //inicializa automaticamente
 	@Autowired
 	private CategoryRepository categoryRepository; //inicializa automaticamente
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -42,7 +46,13 @@ public class TestConfig implements CommandLineRunner{ //executa automaticamente 
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers"); 
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	}
 
 }
